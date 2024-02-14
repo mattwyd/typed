f9995138b2a80cd8a5946ee7cadba058039cd6b9
matt <55467261+mattwyd@users.noreply.github.com>
2024-02-13 22:11:31 -0500

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Typography from '@mui/material/Typography'
+import './App.css'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Typography> Most of our components will be either combinations or customizations of material ui components p sure</Typography>
+      
+    </>
+  )
+}
+
+export default App
