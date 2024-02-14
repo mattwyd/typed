7bd10d910dea46cf6e55d3ac4ba1355e85c7f5b7
matt <55467261+mattwyd@users.noreply.github.com>
2024-02-14 13:18:19 -0500

    added directories

--- src/components/keyPressDisplay.tsx
+++ src/components/keyPressDisplay.tsx
@@ -1,12 +1,3 @@
 import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 
-function KeyPressDisplay(){
-    const [keyPressed, setKeyPressed] = useState('');
-    
-    useEffect(()=> {
-        const handleKeyPress = (event)=> {
-            setKeyPressed(event.key);
-        }
-    })
-}
